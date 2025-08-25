8df4c281d35481d507bc341ed7c7673ee90d8f7a
Alexander von Below <alex@vonbelow.com>
2025-08-25 07:56:22 +0200

    Initial Commit

---
+++ HelloFoMo/HelloFoMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFoMoApp.swift
+//  HelloFoMo
+//
+//  Created by Alexander von Below on 25.08.25.
+//
+
+import SwiftUI
+
+@main
+struct HelloFoMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
